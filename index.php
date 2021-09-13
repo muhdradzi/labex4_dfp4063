0af95dd1839adfa357da548870cfa331fa94fca3
muhdradzi <aizawatakeru99@gmail.com>
2021-09-13 14:57:12 +0800

    try edit index

---
+++ index.php
@@ -0,0 +1,26 @@
+<html>
+
+<head>
+    <title>Radzi LabEx4</title>
+</head>
+
+<body>
+    <h2>Terminology</h2>
+
+    <?php
+    if (isset($_GET['add']))
+    ?>
+
+    <table border="1">
+        <tr>
+            <th>Terms</th>
+            <th>Description</th>
+        </tr>
+        <tr>
+            <td><a href="index.php">[ Add New ]</a></td>
+            <td></td>
+        </tr>
+    </table>
+</body>
+
+</html>
